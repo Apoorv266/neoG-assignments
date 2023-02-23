bf4164a7ec6c624e4d8f7d8f33c23cc4d4a8092c
Apoorv Sharma <apoorvsharma494@gmail.com>
2023-02-23 15:14:23 +0530

    commit

---
+++ set 2/10-concatStr.js
@@ -0,0 +1,5 @@
+// Your ES6 code here
+const concatenateStrings = (str1, str2) => `${str1}${str2}`
+
+
+console.log(concatenateStrings("hello", "world")); // "helloworld"
---
+++ set 2/13-elemSum.js
@@ -0,0 +1,3 @@
+const sumFirstElement = (arr, val) => arr[0] + val
+
+console.log(sumFirstElement([1, 2, 3], 5)); // 6
