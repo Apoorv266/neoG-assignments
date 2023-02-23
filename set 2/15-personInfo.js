590f429efcd3d946e9b49837c00da96d7e7fa9be
Apoorv Sharma <apoorvsharma494@gmail.com>
2023-02-23 15:16:43 +0530

    commit

--- set 2/15-personInfo.js
+++ set 2/15-personInfo.js
@@ -2,6 +2,6 @@ const person1 = { name: 'John', age: 25, occupation: 'Software Engineer' }
 const person2 = { name: 'Jane', age: 45, occupation: 'Data Analyst' }
 
 
-const personInfo = ({name , age}) => `${name} is ${age} years old`
+const personInfo = ({name , age, occupation}) => `${name} is ${age} years old and work as ${occupation}`
 console.log(personInfo(person1)) // Expected output: "John is 25 years old."
 console.log(personInfo(person2)) // Expected output: "Jane is 45 years old."
