44dcd3af7d34df94c164a6b9a48bc0c6db1b6a2b
Apoorv Sharma <apoorvsharma494@gmail.com>
2023-02-26 01:05:16 +0530

    commit

---
+++ set3/15-printProductDetails.js
@@ -0,0 +1,13 @@
+// Your ES6 code here
+const printProductDetails = ({name,specification : {size} , specification : {capacity}}) => `${name} which is of ${size}, has a capacity of ${capacity}`
+
+
+const product = {
+    name: "Apple MacBook Air 2020",
+      specification: {
+          capacity: "256GB",
+        size: "13.3 Inch"
+      },
+    price: 82900
+  }
+  console.log(printProductDetails(product)); // Apple MacBook Air 2020 which is of 13.3 Inch, has a capacity of 256GB.
