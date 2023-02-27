6c264b401d70125fac481bee8ea4e5b36b36ac59
Apoorv Sharma <apoorvsharma494@gmail.com>
2023-02-27 16:06:52 +0530

    commit answer

---
+++ set 4/7-podAndTeamAllocation.js
@@ -0,0 +1,8 @@
+// Your ES6 code here
+
+
+
+
+const userData = { firstName : "John", lastName: "Dee" }
+console.log(podAndTeamAllocation(userData))
+// {firstName: 'John', lastName: 'Dee', teamId: 667543}
