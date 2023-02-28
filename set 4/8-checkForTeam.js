7769404ad2b49bd4b79be9013cb701ad5cb2ceb7
Apoorv Sharma <apoorvsharma494@gmail.com>
2023-02-28 13:04:24 +0530

    commit question

--- set 4/8-checkForTeam.js
+++ set 4/8-checkForTeam.js
@@ -5,25 +5,26 @@ const checkForTeam = (obj) => obj.hasOwnProperty("team") ? obj : {...obj, "team"
 
 // without inbuilt Function
 const checkForTeam1 = (obj) =>{
-    for (const item in obj) {
-        // if (item.team == undefined) {
-            // return {...obj, "team" : "A"}
-            console.log(item);
-        // }
-    }
-    // return "ok"
+   if ("team" in obj) return obj
+   else {
+    return {...obj, team: "A"}
+   }
 }
 
 
 
-// console.log(checkForTeam({ firstName: 'Penn', lastName: 'Ma' }))
-// // {firstName: 'Penn', lastName: 'Ma', team: A}
-// console.log(checkForTeam({ firstName: 'John', lastName: 'Dee', team: "B" }))
-// // {firstName: 'John', lastName: 'Dee', team: B}
-// console.log(checkForTeam({ firstName: 'Priya', lastName: 'Raj' }))
-// // {firstName: 'Priya', lastName: 'Raj', team: A}
-
+console.log(checkForTeam({ firstName: 'Penn', lastName: 'Ma' }))
+// {firstName: 'Penn', lastName: 'Ma', team: A}
+console.log(checkForTeam({ firstName: 'John', lastName: 'Dee', team: "B" }))
+// {firstName: 'John', lastName: 'Dee', team: B}
+console.log(checkForTeam({ firstName: 'Priya', lastName: 'Raj' }))
+// {firstName: 'Priya', lastName: 'Raj', team: A}
+console.log(checkForTeam({ firstName: 'John', lastName: 'Wee' }))
+console.log("------------------------------------------")
 console.log(checkForTeam1({ firstName: 'Penn', lastName: 'Ma' }))
 // {firstName: 'Penn', lastName: 'Ma', team: A}
 console.log(checkForTeam1({ firstName: 'John', lastName: 'Dee', team: "B" }))
 // {firstName: 'John', lastName: 'Dee', team: B}
+console.log(checkForTeam1({ firstName: 'Priya', lastName: 'Raj' }))
+// // {firstName: 'Priya', lastName: 'Raj', team: A}
+console.log(checkForTeam1({ firstName: 'John', lastName: 'Wee' }))
