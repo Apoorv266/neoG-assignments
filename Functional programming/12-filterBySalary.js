4e7fcf0b456d037fe91e960e5928dde669fcce64
Apoorv Sharma <apoorvsharma494@gmail.com>
2023-03-02 15:48:55 +0530

    commit answer

--- Functional programming/12-filterBySalary.js
+++ Functional programming/12-filterBySalary.js
@@ -1,17 +1,17 @@
 // Your ES6 code here
-const filterBySalary = (array) =>{
-let newArr = array.filter(item => item.age > 25 && item.salary > 22000 )
-return newArr
+const filterBySalary = (array) => {
+    let newArr = array.filter(item => item.age > 25 && item.salary > 22000)
+    return newArr
 }
 
 
 const employees = [
-    {name: "Abhay", age: 25, salary: 20000},
-    {name: "Joice", age: 30, salary: 35000},
-    {name: "Reena", age: 35, salary: 15000},
-    {name: "Jeena", age: 40, salary: 50000}
-  ];
-  
-  const filteredEmployees = filterBySalary(employees, 22000, 25);
-  console.log(filteredEmployees);
+    { name: "Abhay", age: 25, salary: 20000 },
+    { name: "Joice", age: 30, salary: 35000 },
+    { name: "Reena", age: 35, salary: 15000 },
+    { name: "Jeena", age: 40, salary: 50000 }
+];
+
+const filteredEmployees = filterBySalary(employees, 22000, 25);
+console.log(filteredEmployees);
   // Output: [{name: "Joice", age: 30, salary: 35000}, {name: "Jeena", age: 40, salary: 50000}]
