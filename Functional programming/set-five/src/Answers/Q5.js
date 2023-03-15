b01fc2d4d3065141ec2c961ad9672a9ac41f500e
Apoorv Sharma <apoorvsharma494@gmail.com>
2023-03-15 16:09:47 +0530

    commit

---
+++ Functional programming/set-five/src/Answers/Q5.js
@@ -0,0 +1,22 @@
+// Create an array of objects representing recipes in a cookbook. Each object has the following properties: id, recipe name, recipe creator name. Write a React component that takes the array of recipes as input and returns an unordered list, where each list item displays the recipe's name and recipe creator name.
+
+import React from 'react'
+
+const Q5 = () => {
+    const recipes = [
+        { id: 1, name: 'Pasta with Tomato Sauce', creator: 'John Smith' },
+        { id: 2, name: 'Roast Chicken', creator: 'Jane Doe' },
+        { id: 3, name: 'Vegetable Curry', creator: 'Bob Johnson' },
+        { id: 4, name: 'Chocolate Cake', creator: 'Mary Davis' },
+        { id: 5, name: 'Caesar Salad', creator: 'Tom Lee' },
+      ];
+  return (
+    <ul>
+        {recipes.map((item) =>(
+            <li><b>Recipe's name  : </b>{item.name} - <b>Creator : </b>{item.creator}</li>
+        ))}
+    </ul>
+  )
+}
+
+export default Q5
--- Functional programming/set-five/src/App.js
+++ Functional programming/set-five/src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 // import Example from './Answers/Example-question';
 // import Q1 from './Answers/Q1';
 // import Q3 from './Answers/Q3';
-import Q4 from './Answers/Q4';
+// import Q4 from './Answers/Q4';
+import Q5 from './Answers/Q5';
 
 function App() {
   return (
     <div className="App">
-     {<Q4/>}
+     {<Q5/>}
     </div>
   );
 }
