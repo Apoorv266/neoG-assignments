88c548fc1a82e8e29695d2c04949d52061c698b9
Apoorv Sharma <apoorvsharma494@gmail.com>
2023-03-15 16:13:47 +0530

    commit answers

---
+++ Functional programming/set-five/src/Answers/Q6.js
@@ -0,0 +1,23 @@
+// Create an array of objects representing cars in a dealership. Each object has the following properties: id, make, model, and price. Write a React component that takes the array of cars as input and returns an unordered list of cars, where each list item displays the car's make, model, and price.
+
+import React from 'react'
+
+const Q6 = () => {
+    const cars = [
+        { id: 1, make: 'Toyota', model: 'Camry', price: 25000 },
+        { id: 2, make: 'Honda', model: 'Accord', price: 28000 },
+        { id: 3, make: 'Ford', model: 'Mustang', price: 35000 },
+        { id: 4, make: 'Chevrolet', model: 'Camaro', price: 40000 },
+        { id: 5, make: 'Tesla', model: 'Model 3', price: 50000 },
+      ];
+
+      const displayList = item => <li>{item.make} - {item.model} - (Rs.{item.price})</li>
+
+  return (
+    <ul>
+        {cars.map(displayList)}
+    </ul>
+  )
+}
+
+export default Q6
--- Functional programming/set-five/src/App.js
+++ Functional programming/set-five/src/App.js
@@ -4,12 +4,13 @@ import './App.css';
 // import Q1 from './Answers/Q1';
 // import Q3 from './Answers/Q3';
 // import Q4 from './Answers/Q4';
-import Q5 from './Answers/Q5';
+// import Q5 from './Answers/Q5';
+import Q6 from './Answers/Q6';
 
 function App() {
   return (
     <div className="App">
-     {<Q5/>}
+     {<Q6/>}
     </div>
   );
 }
