7d5def66c61d7009e484394be0fe295d577e650a
Apoorv Sharma <apoorvsharma494@gmail.com>
2023-03-31 01:45:54 +0530

    commit

--- Practice Set questions/React/set-one/src/Answers/DisplayData.js
+++ Practice Set questions/React/set-one/src/Answers/DisplayData.js
@@ -1,15 +1,45 @@
-import React from 'react'
+import React from "react";
 
 const DisplayData = ({ products }) => {
-    return (
-        <>
-            <ul>
-                {products.filter((item) => item.sales > item.quantity).map((item, index) => {
-                    return (<li key={index}>Name : {item.name},  Quantity : {item.quantity},  Sales : {item.sales}</li>)
-                })}
-            </ul>
-        </>
-    )
-}
+  return (
+    <>
+      <ul>
+        {products
+          .filter((item) => item.sales > item.quantity)
+          .map((item, index) => {
+            return (
+              <li key={index}>
+                Name : {item.name}, Quantity : {item.quantity}, Sales :{" "}
+                {item.sales}
+              </li>
+            );
+          })}
+        <div>-------------------------------------</div>
+        {/* or */}
+        {products.map((item, index) => {
+          return item.sales > item.quantity ? (
+            <li key={index}>
+              Name : {item.name}, Quantity : {item.quantity}, Sales :{" "}
+              {item.sales}
+            </li>
+          ) : null;
+        })}
+        <div>-------------------------------------</div>
+        {/* or */}
+        {products.map((item, index) => {
+          if (item.sales > item.quantity) {
+            return <li key={index}>
+            Name : {item.name}, Quantity : {item.quantity}, Sales :{" "}
+            {item.sales}
+          </li>
+          }
+          else{
+            return null
+          }
+        })}
+      </ul>
+    </>
+  );
+};
 
-export default DisplayData
+export default DisplayData;
