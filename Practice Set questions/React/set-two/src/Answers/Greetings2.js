16cf56e39e773f1dff96bbc4f07671f715b1db5e
Apoorv Sharma <apoorvsharma494@gmail.com>
2023-04-01 23:23:39 +0530

    commit

---
+++ Practice Set questions/React/set-two/src/Answers/Greetings2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Greetings2 = () => {
+  return (
+    <div>Greetings2</div>
+  )
+}
+
+export default Greetings2
--- Practice Set questions/React/set-two/src/App.js
+++ Practice Set questions/React/set-two/src/App.js
@@ -1,13 +1,18 @@
-import Name1 from './Answers/Name1';
 import './App.css';
+import Greetings2 from './Answers/Greetings2';
+// import Name1 from './Answers/Name1';
 
 function App() {
   //1st
-  const product = "Washing machine"
-  const price = 25000
+  // const product = "Washing machine"
+  // const price = 25000
+
+  //2nd 
+  const name = "Apoorv"
   return (
     <div className="App">
-    <Name1 product={product} price={price}/>
+    {/* <Name1 product={product} price={price}/> */}
+    <Greetings2 name={name}/>
     </div>
   );
 }
