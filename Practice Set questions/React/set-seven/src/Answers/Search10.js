3fb68834f71a2abfb36d02149613d1043c007390
Apoorv Sharma <apoorvsharma494@gmail.com>
2023-04-11 03:45:43 +0530

    commit

--- Practice Set questions/React/set-seven/src/Answers/Search10.js
+++ Practice Set questions/React/set-seven/src/Answers/Search10.js
@@ -11,7 +11,7 @@ const Search10 = () => {
                         data: {
                             products: [
                                 { name: "Color Pencils", price: 50, quantity: 40, rating: 4.5 },
-                                { name: "Sketch Pens", price: 110, quantity: 20, rating: 3.8 },
+                                { name: "Sketchpens", price: 110, quantity: 20, rating: 3.8 },
                                 { name: "Eraser", price: 20, quantity: 20, rating: 4.2 },
                                 { name: "Sharpener", price: 22, quantity: 30, rating: 4.7 }
                             ]
@@ -30,29 +30,48 @@ const Search10 = () => {
 
     const [data, setdata] = useState([])
     const [inputValue, setinputValue] = useState("")
+    const [filteredProducts, setfilteredProducts] = useState([])
 
 
     const fetchFunc = async () => {
         const response = await fakeFetch("https://example.com/api/products")
         if (response.status === 200) {
             setdata(response.data.products)
+            setfilteredProducts(response.data.products)
         }
     }
 
     useEffect(() => {
         fetchFunc()
     }, [])
+
+
+    const handleChange = (e) => {
+        let value = e.target.value
+        if (value === "") {
+            setfilteredProducts(data)
+            setinputValue(() => value)
+        } else {
+            setinputValue(() => value)
+            let array = data.filter((item) => item.name.toLowerCase().includes(inputValue))
+            setfilteredProducts(array)
+        }
+    }
+
+    // alternate way
     
-    const handleSearch = (event) => {
-        setinputValue(event.target.value.toLowerCase());
-      };
-    
-      const filteredProducts = data.filter((product) =>
-        product.name.toLowerCase().includes(inputValue)
-      );
+// as the setinputValue will fire the inputValue will be set and component will rerender which will create new filteredProducts on every render, and then we will map filteredProducts
+
+    //   const handleChange = (event) => {
+    //     setinputValue(event.target.value.toLowerCase());
+    //   };
+
+    //   const filteredProducts = data.filter((product) =>
+    //     product.name.toLowerCase().includes(inputValue)
+    //   );
     return (
         <div>
-            <input type='text' value={inputValue} onChange={handleSearch}></input>
+            <input type='text' value={inputValue} onChange={handleChange}></input>
             {filteredProducts.map((item) => {
                 return (
                     <div>
