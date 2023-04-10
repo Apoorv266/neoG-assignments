24405776b0d352108dc7f10f8f6c6ecc60af22b4
Apoorv Sharma <apoorvsharma494@gmail.com>
2023-04-10 18:28:47 +0530

    commit

--- Practice Set questions/React/set-six/src/Answers/DispImg1.js
+++ Practice Set questions/React/set-six/src/Answers/DispImg1.js
@@ -46,13 +46,19 @@ const DispImg1 = () => {
   
   const [data, setdata] = useState([]);
   const [btnData, setbtnData] = useState([]);
-  let array = [...data]
+  const [filterValue, setfilterValue] = useState(null)
 
+const filterFunc = ()=>{
+  if (filterValue !== null) {
+    return data.filter(item => item.name === filterValue)
+  }else{
+    return data
+  }
+}
   const fetchFunc = async () => {
     const response = await fakeFetch("https://example.com/api/products");
-    if (response.status === 200) {
-        array = response.data.products
-      setdata(array);
+    if (response.status === 200) { 
+      setdata(response.data.products);
       setbtnData(response.data.products.map(item => item.name));
     }
   };
@@ -62,24 +68,22 @@ const DispImg1 = () => {
   }, []);
 
   const handleClick = (item) => {
-    let newArray = array.filter((value) => value.name === item);
-    setdata(newArray);
-    console.log(array)
+    setfilterValue(item)
   };
   return (
     <>
       <div>
-        {btnData.map((item, index) => (
+        {btnData.map((item) => (
           <button onClick={() => handleClick(item)}>Show {item}</button>
         ))}
       </div>
       <div>
-        {data.map((item, index) => {
+        {filterFunc().map((item, index) => {
           return (
             <div key={index}>
               <img src={item.src} alt="" srcset="" />
               <h4>Name : {item.name}</h4>
-              <h4>Price :Rs. {item.price}</h4>
+              <h4>Price : Rs. {item.price}</h4>
               <h4>Description : {item.desc}</h4>
             </div>
           );
--- Practice Set questions/React/set-six/src/App.js
+++ Practice Set questions/React/set-six/src/App.js
@@ -1,5 +1,5 @@
 
-// import DispImg1 from './Answers/DispImg1';
+import DispImg1 from './Answers/DispImg1';
 // import HabitTracker3 from './Answers/HabitTracker3';
 // import Label9 from './Answers/Label9';
 // import Posts5 from './Answers/Posts5';
@@ -7,14 +7,14 @@
 // import Todos2 from './Answers/Todos2';
 // import Unarchived6 from './Answers/Unarchived6';
 // import VideoLibrary4 from './Answers/VideoLibrary4';
-import SocialMedia10 from './Answers/SocialMedia10';
+// import SocialMedia10 from './Answers/SocialMedia10';
 // import UserProfile8 from './Answers/UserProfile8';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* <DispImg1/> */}
+      <DispImg1/>
       {/* <Todos2/> */}
       {/* <HabitTracker3/> */}
       {/* <VideoLibrary4/> */}
@@ -23,7 +23,7 @@ function App() {
       {/* <Projects7/> */}
       {/* <UserProfile8 /> */}
       {/* <Label9/> */}
-      <SocialMedia10/>
+      {/* <SocialMedia10/> */}
     </div>
   );
 }
