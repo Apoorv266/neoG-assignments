8aeb74b524e847d6182afffab17d04089c53cd87
Apoorv Sharma <apoorvsharma494@gmail.com>
2023-04-22 15:52:50 +0530

    commit

--- Practice Set questions/React/set-eight/src/Answers/Ecom App/Cart.js
+++ Practice Set questions/React/set-eight/src/Answers/Ecom App/Cart.js
@@ -3,14 +3,17 @@ import { useContext } from "react";
 import { cartContext } from "./EcomHome";
 
 const Cart = () => {
-  const { data, dltFrmCartFunc } = useContext(cartContext);
+  const { data, dltFrmCartFunc , cartNo} = useContext(cartContext);
+  let emptyCart = true
   return (
     <div>
+      {cartNo !== 0 && <h1>Total Items : {cartNo}</h1>}
       <h1>Cart Listing</h1>
-      {data ? data.map((item) => {
+      {data.map((item) => {
         if (item.cart) {
           return (
             <div key={item.id}>
+              {emptyCart = false}
               <h3>{item.name}</h3>
               <p>{item.description}</p>
               <p>Price : Rs. {item.price}</p>
@@ -19,12 +22,9 @@ const Cart = () => {
               <hr />
             </div>
           )
-        }else{
-          return (
-            null
-          )
-        }
-      }) : <h3>No items in cart</h3>}
+        }else return null
+      })}
+      {emptyCart && <h3>No items in cart</h3>}
     </div>
   )
 }
--- Practice Set questions/React/set-eight/src/Answers/Ecom App/CartContext.js
+++ Practice Set questions/React/set-eight/src/Answers/Ecom App/CartContext.js
@@ -5,6 +5,7 @@ export const cartContext = createContext()
 
 const CartContextProvider = ({ children }) => {
   const [data, setdata] = useState([])
+  const [cartNo, setcartNo] = useState(0)
 
   const fetchFunc = async () => {
     const response = await fakeFetch("https://example.com/api/products")
@@ -20,16 +21,18 @@ const CartContextProvider = ({ children }) => {
   const addTocartFunc = (currId) =>{
     let cartArray = data.map((item) => item.id === currId ? {...item, cart: true} : item)
     setdata(cartArray) 
+    setcartNo((cartNo) => cartNo + 1)
   }
 
 
   const dltFrmCartFunc = (currId) =>{
     let dltcartArray = data.map((item) => item.id === currId ? {...item, cart: false} : item)
     setdata(dltcartArray)
+    setcartNo((cartNo) => cartNo - 1)
   }
 
   return (
-    <cartContext.Provider value={{ data , addTocartFunc, dltFrmCartFunc}}>
+    <cartContext.Provider value={{ data , addTocartFunc, dltFrmCartFunc, cartNo}}>
       {children}
     </cartContext.Provider>
   )
