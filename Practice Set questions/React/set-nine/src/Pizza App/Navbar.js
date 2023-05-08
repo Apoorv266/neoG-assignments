d38332fda999162f8e37f35c0b7f5dc4e7ceb24a
Apoorv Sharma <apoorvsharma494@gmail.com>
2023-05-08 23:59:48 +0530

    commit

--- Practice Set questions/React/set-nine/src/Pizza App/Navbar.js
+++ Practice Set questions/React/set-nine/src/Pizza App/Navbar.js
@@ -2,13 +2,13 @@ import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { pizzaContext } from './PizzaContext'
 
-const Navbar = ({cartData}) => {
-  const {us} = useContext(pizzaContext)
+const Navbar = () => {
+  const {cartData} = useContext(pizzaContext)
   return (
     <div>
         <Link to={"/"}>Home</Link>{" "}
         <Link to={"/menu"}>Menu</Link>{" "}
-        <Link to={"/cart"}>Cart</Link>{" "}
+        <Link to={"/cart"}>Cart {cartData.length}</Link>{" "}
     </div>
   )
 }
