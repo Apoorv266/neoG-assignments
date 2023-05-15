f810d9f82b8a09d9ab7cc1e1ed7afdc64e9f961d
Apoorv Sharma <apoorvsharma494@gmail.com>
2023-05-15 20:22:30 +0530

    feat - added spam and read functionality

--- Practice Set questions/React/set-ten/src/Components/Inbox.js
+++ Practice Set questions/React/set-ten/src/Components/Inbox.js
@@ -3,23 +3,29 @@ import { emailContext } from "../Contexts/EmailContext";
 import "../Styles/MsgCard.css";
 
 const Inbox = () => {
-  const { emailData } = useContext(emailContext);
+  const { emailData, dispatch } = useContext(emailContext);
   return (
     <>
+    {emailData.length === 0 && <h1 style={{width: "80vw"}}>No emails to display</h1>}
       {emailData.map((item) => {
         return (
-          <div className="mainCard">
+          <div className="mainCard" key={item.mId} style={{backgroundColor:item.unread ? "#F1F6FC" :"white"}}>
             <div id={item.id} className="header">
               <h3>Subject : {item.subject}</h3>
-              <p>Star</p>
+              <p onClick={()=>dispatch({ type: "STAR", payload: item.mId })}>{!item.isStarred ? "Star": "Unstar"}</p>
             </div>
             <p>{item.content}</p>
             <div className="cardBtn">
               <button className="btn0">View Details</button>
               <div className="utils-btn">
-                <button className="btn1">Delete</button>
-                <button className="btn2">Mark as read</button>
-                <button className="btn3">Report as spam</button>
+                <button
+                  className="btn1"
+                  onClick={() => dispatch({ type: "DELETE", payload: item.mId })}
+                >
+                  Delete
+                </button>
+                <button className="btn2" onClick={()=>dispatch({ type: "READ", payload: item.mId })} style={{color:item.unread ? "orange": "black"}}>{item.unread ? "Mark as read" : "Mark as Unread"}</button>
+                <button className="btn3" onClick={()=>dispatch({ type: "SPAM", payload: item.mId })} style={{color:item.isSpam ? "green": "black"}}>{item.isSpam ? "Marked as spam" : "Report as spam"}</button>
               </div>
             </div>
           </div>
--- Practice Set questions/React/set-ten/src/Contexts/EmailContext.js
+++ Practice Set questions/React/set-ten/src/Contexts/EmailContext.js
@@ -1,13 +1,29 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useReducer } from 'react'
 import { mails } from '../Data/Data'
 
 
 export const emailContext = createContext()
-const EmailContextFunc = ({children}) => {
-    const [emailData, setemailData] = useState(mails)
-    
+const EmailContextFunc = ({ children }) => {
+
+  const reducerFunc = (state, action) => {
+    switch (action.type) {
+      case "DELETE":
+        return state.filter(item => item.mId !== action.payload)
+      case "STAR":
+        return state.map((item) => item.mId === action.payload ? { ...item, isStarred: !item.isStarred } : item)
+      case "READ":
+        return state.map((item) => item.mId === action.payload ? { ...item, unread: !item.unread } : item)
+      case "SPAM":
+        return state.map((item) => item.mId === action.payload ? item.hasOwnProperty("isSpam") ? { ...item, isSpam: !item.isSpam } : { ...item, isSpam: true } : item)
+      default:
+        return state
+    }
+  }
+  const [emailData, dispatch] = useReducer(reducerFunc, mails)
+
+
   return (
-    <emailContext.Provider value={{emailData}}>{children}</emailContext.Provider>
+    <emailContext.Provider value={{ emailData, dispatch }}>{children}</emailContext.Provider>
   )
 }
 
