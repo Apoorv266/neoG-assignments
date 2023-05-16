d3bffca2a3f616e3d4c31337f4dff444e63d2f52
Apoorv Sharma <apoorvsharma494@gmail.com>
2023-05-16 17:47:49 +0530

    commit

--- Practice Set questions/React/set-ten/src/index.js
+++ Practice Set questions/React/set-ten/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import EmailContextFunc from './Contexts/EmailContext';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
